73ea78c0fd243801dd0b9edb9588d4b49d59cab7
Natdanai Pinaves <jgogo012@gmail.com>
2025-07-15 10:43:46 +0700

    Fix: Footer get current year.

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,10 +1,16 @@
+const getFooterYear = () => {
+  const currentYear = new Date().getFullYear();
+  return currentYear;
+}
+
 export default function Footer() {
+  const year = getFooterYear();
   return (
       <>
         <footer className="flex-shrink-0 py-8 mt-auto">
           <div className="container mx-auto px-6">
             <div className="text-center text-sm text-gray-500 font-light">
-              <p>© 2024 มหาวิทยาลัยเกษตรศาสตร์</p>
+              <p>© {year} มหาวิทยาลัยเกษตรศาสตร์</p>
               <p className="mt-2">
                 พัฒนาโดย ฝ่ายระบบคอมพิวเตอร์และเครือข่าย สำนักบริการคอมพิวเตอร์ มหาวิทยาลัยเกษตรศาสตร์
               </p>
